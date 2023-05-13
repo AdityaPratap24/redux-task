358f10d671316958b705ebd2a6a54fbf3b74f43e
AdityaPratap24 <adityapratap2401@gmail.com>
2023-05-13 15:58:23 +0530

    second task

--- redux-demo.js
+++ redux-demo.js
@@ -1,15 +1,37 @@
-const redux = require("redux");
+const redux = require('redux');
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
-};
+    if (action.type == 'increment') {
+        return {
+            counter: state.counter + 1
+        };
+    }
+    if (action.type == 'decrement') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+    if (action.type == 'INCREMENTBY2') {
+        return {
+            counter: state.counter + 2
+        };
+    }
+    if (action.type == 'DECREMENTBY2') {
+        return {
+            counter: state.counter - 2
+        };
+    }
+return state;
+}
 const store = redux.legacy_createStore(counterReducer);
-console.log(store.getState());
+
 const counterSubscriber = () => {
-  const lateststate = store.getState();
-  console.log(lateststate);
-};
+    const lateststate = store.getState();
+    console.log(lateststate);
+}
 store.subscribe(counterSubscriber);
-store.dispatch({ type: "increment" });
+
+store.dispatch({ type: "increment" })
+store.dispatch({ type: "decrement" })
+store.dispatch({ type: "INCREMENTBY2" })
+store.dispatch({ type: "DECREMENTBY2" })
